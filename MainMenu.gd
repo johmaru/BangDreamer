88775e6d8600a6afc7018272efe1e412be875408
Jo Matsumaru <hatunehimeka@outlook.jp>
2023-07-28 00:40:57 +0900

    ディスプレイ設定追加 + Escapeキーでゲームを終了出来るように + 翻訳追加

--- MainMenu.gd
+++ MainMenu.gd
@@ -15,3 +15,16 @@ func _on_exit_pressed():
 
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://MainMenuControlScene/SettingsMenu.tscn")
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			$QuitMenu.visible = true
+
+
+func _on_quit_yes_button_pressed():
+	get_tree().quit()
+
+
+func _on_quit_no_button_pressed():
+	$QuitMenu.visible = false
--- MainMenuControlScene/SettingsMenu.gd
+++ MainMenuControlScene/SettingsMenu.gd
@@ -3,10 +3,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	set_process(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
@@ -22,3 +22,40 @@ func _on_lang_button_item_selected(index):
 			TranslationServer.set_locale("en-US")
 		1:
 			TranslationServer.set_locale("ja-JP")	
+
+
+func _on_option_button_item_selected(index):
+	
+	match index:
+		
+		0:
+			#Exact Resolution is 1920x1020
+			DisplayServer.window_set_size(Vector2i(1920,1020))
+			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+		1:
+			DisplayServer.window_set_size(Vector2i(1600,900))
+			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+		2: 
+			DisplayServer.window_set_size(Vector2i(1280,768))
+			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+
+
+func _on_resized():
+	pass # Replace with function body.
+
+
+func _on_full_screen_button_toggled(button_pressed):
+	
+	match button_pressed:
+		true:
+			
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			
+		false:
+			
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			get_tree().change_scene_to_file("res://MainMenu.tscn")
