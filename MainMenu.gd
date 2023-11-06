e3d9b7ea7e07a657ec0b77471cf24067b1bafe45
Jo Matsumaru <hatunehimeka@outlook.jp>
2023-11-06 22:37:56 +0900

    設定の保存機能完全追加

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,17 +1,26 @@
 extends Control
+var mainInitilize = 0
 var WindowSize = {}
-const ConfigPath := "user://Config.json"
-const ConfigFilePath := "user://Config.cfg"
+var nowLanguage = {}
+var WindowScreenMode = {}
+var ConfigFilePath := OS.get_executable_path().get_base_dir() +  "/Config.cfg"
 var GameConfigPath := OS.get_executable_path().get_base_dir() + "/gameSettings.cfg"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MarginContainer/VBoxContainer/NewGame.grab_focus()
 	print(WindowSize)
-	match FileAccess.file_exists(ConfigFilePath):
-		true:
-			pass
-		false:
-			initConfigFile()
+	
+	match  mainInitilize:
+		0:
+			match FileAccess.file_exists(ConfigFilePath):
+				true:
+					SetGameConfig()
+					WindowsizeInitilize()
+					LanguageInitilize()
+					FullScreenInitilize()
+					mainInitilize = 1
+				false:
+					initConfigFile()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
@@ -48,4 +57,50 @@ func initConfigFile() -> void:
 	
 	config.set_value("Setting", "gameScreenSize", "2")
 	config.set_value("Setting", "Lang", "en")
+	config.set_value("Setting", "FullScreenMode", "0")
 	config.save(ConfigFilePath)
+	
+func SetGameConfig() -> void:
+	var config = ConfigFile.new()
+	var err = config.load(ConfigFilePath)
+	
+	if err != OK:
+		return
+		
+	for settings in config.get_sections():
+		var lang = config.get_value(settings, "Lang")
+		var ScreenSize = config.get_value(settings, "gameScreenSize")
+		var ScreenMode = config.get_value(settings, "FullScreenMode")
+		WindowSize = ScreenSize
+		nowLanguage = lang
+		WindowScreenMode = ScreenMode
+		
+func WindowsizeInitilize():
+	match WindowSize:
+		
+		"0":
+			#Exact Resolution is 1920x1020
+			DisplayServer.window_set_size(Vector2i(1920,1020))
+			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+		"1":
+			DisplayServer.window_set_size(Vector2i(1600,900))
+			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+		"2": 
+			DisplayServer.window_set_size(Vector2i(1280,768))
+			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+			
+func LanguageInitilize():
+	match nowLanguage:
+		"en":
+			TranslationServer.set_locale("en-US")
+		"ja":
+			TranslationServer.set_locale("ja-JP")
+
+func FullScreenInitilize():
+	match WindowScreenMode:
+		"1":
+			
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		"0":
+			
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
--- MainMenuControlScene/SettingsMenu.gd
+++ MainMenuControlScene/SettingsMenu.gd
@@ -1,6 +1,5 @@
 extends Control
-const 	ConfigPath := "user://Config.json"
-const ConfigFilePath := "user://Config.cfg"
+var ConfigFilePath := OS.get_executable_path().get_base_dir() +  "/Config.cfg"
 var config = ConfigFile.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,10 +21,12 @@ func _on_lang_button_item_selected(index):
 	match index:
 		0:
 			TranslationServer.set_locale("en-US")
+			config.load(ConfigFilePath)
 			config.set_value("Setting", "Lang", "en")
 			config.save(ConfigFilePath)
 		1:
 			TranslationServer.set_locale("ja-JP")
+			config.load(ConfigFilePath)
 			config.set_value("Setting", "Lang", "ja")
 			config.save(ConfigFilePath)
 
@@ -37,16 +38,19 @@ func _on_option_button_item_selected(index):
 			#Exact Resolution is 1920x1020
 			DisplayServer.window_set_size(Vector2i(1920,1020))
 			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+			config.load(ConfigFilePath)
 			config.set_value("Setting", "gameScreenSize", "0")
 			config.save(ConfigFilePath)
 		1:
 			DisplayServer.window_set_size(Vector2i(1600,900))
 			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+			config.load(ConfigFilePath)
 			config.set_value("Setting", "gameScreenSize", "1")
 			config.save(ConfigFilePath)
 		2: 
 			DisplayServer.window_set_size(Vector2i(1280,768))
 			DisplayServer.window_set_position(Vector2(DisplayServer.screen_get_position()) + DisplayServer.screen_get_size()*0.5 - DisplayServer.window_get_size()*0.5)
+			config.load(ConfigFilePath)
 			config.set_value("Setting", "gameScreenSize", "2")
 			config.save(ConfigFilePath)
 
@@ -61,11 +65,15 @@ func _on_full_screen_button_toggled(button_pressed):
 		true:
 			
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-			
+			config.load(ConfigFilePath)
+			config.set_value("Setting", "FullScreenMode", "1")
+			config.save(ConfigFilePath)
 		false:
 			
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-			
+			config.load(ConfigFilePath)
+			config.set_value("Setting", "FullScreenMode", "0")
+			config.save(ConfigFilePath)
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_ESCAPE:
